94c6e91308fd5dc93dfc57b79693e1b9b7db78a9
Harsh Bhandari <harshbhnadari@Harshs-MacBook-Air.local>
2020-08-14 11:37:14 +0530

    Updated mainfile

--- mainfile.py
+++ mainfile.py
@@ -1,12 +1,12 @@
-ch=input(int(write a number 1. add 2. subtract 3.multiplication 4.division))
+ch=int(input(write a number 1. add 2. subtract 3.multiplication 4.division))
 switch (ch){
-    case 1:
+    case 1:#code to add
 
-    case 2:
+    case 2:#code to subtract
 
-    case 3:
+    case 3:#code to multiply
 
-    case 4:
+    case 4:#code to divide
 
 }
 
