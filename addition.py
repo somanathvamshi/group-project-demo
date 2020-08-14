b1dc91bf9fe6de42102f86ea9f1841faea2b0490
somanathvamshi <somanathvamshi@gmail.com>
2020-08-14 13:19:32 -0700

    adition for branch

--- addition.py
+++ addition.py
@@ -1,4 +1,14 @@
-a=int(input("input any number for addition"))
-b=int(input("input any number for addition"))
-sum = a+b
+a=int(input("input any number  for addition"))#input a
+#input a
+b=int(input("input any number for addition"))#input b
+sum = a+b # addition of a and b
 print("Addition" ,a ,"+" ,b ,"is" ,sum)
+print(" #sfsg")
+print("'''debrbbntrdefrn'''")
+'''hfrfnc vnfl'''
+'''hfrfnc vnfl
+Fasgdhhm''
+
+
+
+
