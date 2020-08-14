6e41568093fe167e7f10c40cc8552cf9a4127ddc
Rajat Sran <rajatsran27@gmail.com>
2020-08-14 14:57:53 +0530

    adding multi

--- multi.py
+++ multi.py
@@ -1,4 +1,4 @@
-a=int(input())
-b=int(input())
-c=int(a*b)
-print(c)
+a=int(input("input number  for multiplication"))#input a
+b=int(input("input number for multiplication"))#input b
+c = a*b # multiplication of a and b
+print("SUBTRAACTION" ,a ,"*" ,b ,"is" ,c)
