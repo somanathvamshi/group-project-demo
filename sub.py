0331482d36ce808c37e0cd3e8808084ac19b7aef
Harsh Bhandari <harshbhnadari@Harshs-MacBook-Air.local>
2020-09-03 10:25:33 +0530

    Harsh commit

--- sub.py
+++ sub.py
@@ -1,4 +1,6 @@
 a=int(input("input number  for subtraction"))#input a
+print("")
+
 b=int(input("input number for subtraction"))#input b
 c = a-b # subtraction of a and b
 print("SUBTRAACTION" ,a ,"-" ,b ,"is" ,c)
